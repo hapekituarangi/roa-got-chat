0b71f6805ae891388dbe1fc54960a5bf8607a589
BearFox <nick.mjac@gmail.com>
2016-05-27 11:30:58 +1200

    add emit and broadcast functionality

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ app.get('/', function(req, res) {
   res.sendfile(__dirname + '/index.html')
 })
 
+var connections = 0
 io.on('connection', function(socket) {
+  connections++
+  socket.broadcast.emit('chat message', 'Someone else joined. There are now ' + connections.toString() + ' people in the room')
+  socket.on('chat message', function(msg) {
+    console.log('message: ', msg)
+    io.emit('chat message', msg)
+  })
   console.log('a user connected')
+  socket.on('disconnect', function() {
+    connections--
+    socket.broadcast.emit('chat message', 'Someone else left. There are now ' + connections.toString() + ' people in the room')
+    console.log('a user disconnected')
+  })
 })
 
 http.listen(3000, function() {
